c3cb2733f05f778b1618c00eb0f67baa77ee21f2
Leritas <din.fuso@mail.ru>
2022-08-05 14:25:35 +0300

    server ready for now, html+css need todo

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.set('view engine', 'ejs')
+
+//Routes for main url
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Kill Music' })
+})
+
+
+
+
+
+//Routers for server
+
+const itemsRouter = require('./routes/items')
+app.use('/items', itemsRouter)
+
+
+//Server start
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
