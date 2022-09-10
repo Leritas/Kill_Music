f89e9bde549e783e819039d065ca85babca82866
EV1L46rus <vislik46rus@gmail.com>
2022-09-10 23:15:53 +0300

    burfer

--- public/script.js
+++ public/script.js
@@ -12,5 +12,9 @@ function showSearchInp() {
             setTimeout( () => {document.querySelector('.search-mobile-header').classList.toggle('active')}, 50)
             setTimeout( () => {document.querySelector('.inp-hide-header').classList.toggle('inp-show-onclick')}, 50)
     }
- 
+}
+
+function menuSlider() {
+    document.querySelector('.block-screen').classList.toggle('active')
+    document.querySelector('.menu-slider').classList.toggle('active')
 }
