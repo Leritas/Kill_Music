06560c81bd76d7ad052d78f155457a84ad3731d9
EV1L46rus <vislik46rus@gmail.com>
2022-09-10 21:57:47 +0300

    animations fixed

--- public/script.js
+++ public/script.js
@@ -4,13 +4,13 @@ function activeBtnCatalog(div) {
 
 function showSearchInp() {
     if (document.querySelector('.inp-show-onclick')) {
-            setTimeout( () => {document.querySelector('.logo-btn-header-mobil').classList.toggle('active')  }, 200)
+            setTimeout( () => {document.querySelector('.logo-btn-header-mobil').classList.toggle('active')  }, 50)
             document.querySelector('.search-mobile-header').classList.toggle('active')
             document.querySelector('.inp-hide-header').classList.toggle('inp-show-onclick')
     } else {
             document.querySelector('.logo-btn-header-mobil').classList.toggle('active')    
-            setTimeout( () => {document.querySelector('.search-mobile-header').classList.toggle('active')}, 200)
-            setTimeout( () => {document.querySelector('.inp-hide-header').classList.toggle('inp-show-onclick')}, 200)
+            setTimeout( () => {document.querySelector('.search-mobile-header').classList.toggle('active')}, 50)
+            setTimeout( () => {document.querySelector('.inp-hide-header').classList.toggle('inp-show-onclick')}, 50)
     }
  
 }
