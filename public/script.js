3ef1c00cb3cd949a4c71bef9b35b9ab848355f5c
EV1L46rus <vislik46rus@gmail.com>
2022-09-10 20:26:39 +0300

    mobile header

--- public/script.js
+++ public/script.js
@@ -1,4 +1,16 @@
 function activeBtnCatalog(div) {
-    console.log(div)
     div.classList.toggle('btnActiveCatalogHeader')
+}
+
+function showSearchInp() {
+    if (document.querySelector('.logo-hide-btn-header-mobil')){
+        document.querySelector('.search-mobile-header').style.width = ""
+        document.querySelector('.logo-btn-header-mobil').style.width = "230px"
+    } else {
+        document.querySelector('.search-mobile-header').style.width = "100%"
+        document.querySelector('.logo-btn-header-mobil').style.width = "0px"
+    }
+    
+    document.querySelector('.logo-btn-header-mobil').classList.toggle('logo-hide-btn-header-mobil')
+    document.querySelector('.inp-hide-header').classList.toggle('inp-show-onclick')
 }
