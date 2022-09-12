2c02bb026d767697b784f2caf7a95f706c2fe06c
leritas <din.fuso@mail.ru>
2022-09-13 00:13:39 +0300

    sqlite3 working properly

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const port = 80
 
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./database/main.db')
+
+db.serialize(() => {
+  db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+      console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
+
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 
