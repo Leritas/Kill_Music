2f014c4e723e3ac4f5753cde9246b69423b60fd9
EV1L46rus <vislik46rus@gmail.com>
2022-10-03 16:14:27 +0300

    admin-panel

--- public/admin.js
+++ public/admin.js
@@ -1,5 +1,11 @@
 window.addEventListener('resize', closeNavigation);
 
+let scrollContener = document.querySelector('.categories')
+scrollContener.addEventListener('wheel', (e) => {
+    e.preventDefault()
+    scrollContener.scrollLeft += e.deltaY
+})
+
 function openNavigation() {
     document.querySelector('.navigation-menu').classList.add('active')
     document.querySelector('.blur-add').classList.add('active')
@@ -18,14 +24,23 @@ function fullOrderMenu(div){
 }
 
 function closeMenu() {
-    document.querySelector('.blur-add').classList.remove('active')
+    document.querySelector('.blur-add.active')?.classList.remove('active')
     document.querySelector('.navigation-menu.active')?.classList.remove('active')
     document.querySelector('.add-admin-menu.active')?.classList.remove('active')
+    document.querySelector('.edit-account.active')?.classList.remove('active')
 }
 
-function menuOpen(){
+function menuOpen(btn){
     document.querySelector('.blur-add').classList.add('active')
-    document.querySelector('.add-admin-menu').classList.add('active')
+    const idPush = btn.id
+    
+    if (idPush === 'edit'){
+        document.querySelector('.edit-account').classList.add('active')
+    }
+    else if (idPush === 'add'){
+        document.querySelector('.add-admin-menu').classList.add('active')
+    }
+    
 }
 
 function showInput(btn){
@@ -43,17 +58,55 @@ function switchScreen(div){
     document.querySelector('.body').querySelector('.active')?.classList.remove('active')
     document.querySelector('.navigation-menu').classList.remove('active')
     document.querySelector('.blur-add').classList.remove('active')
-
-
-    if (resultText === 'Панель управления') {
-        document.querySelector('.main').classList.add('active')
-    }
     
     if (resultText === 'Заказы') {
         document.querySelector('.orders').classList.add('active')
     }
 
+    if (resultText === 'Категории') {
+        document.querySelector('.categories').classList.add('active')
+    }
+
+    if (resultText === 'Товары') {
+        document.querySelector('.items').classList.add('active')
+    }
+
     if (resultText === 'Аккаунты') {
         document.querySelector('.users').classList.add('active')
     }
+}
+
+function unlockInput(span){
+    console.log(span.parentNode.parentNode.querySelectorAll('.show'))
+    span.parentNode.querySelector('input').removeAttribute('disabled')
+    span.parentNode.querySelector('input').setAttribute('autofocus', '')
+    span.querySelector('svg').classList.add('active')
+    span.parentNode.querySelector('.show')?.classList.add('active')
+}
+
+function unlockCatalog(span){
+    span.parentNode.querySelector('.pencil')?.classList.toggle('active')
+    span.parentNode.querySelector('.done')?.classList.toggle('active')
+    span.parentNode.querySelector('input').removeAttribute('disabled')
+}
+
+function lockCatalog(span){
+    span.parentNode.querySelector('.pencil')?.classList.toggle('active')
+    span.parentNode.querySelector('.done')?.classList.toggle('active')
+    span.parentNode.querySelector('input').setAttribute('disabled', '')
+}
+
+function addCategory(span){
+    span.parentNode.parentNode.querySelector('.show-add-menu')?.classList.toggle('active')
+    span.parentNode.parentNode.querySelector('.add')?.classList.toggle('active')
+}
+
+function leaveItem(div){
+    div.querySelector('.active')?.classList.remove('active')
+    div.querySelector('.first').classList.toggle('active')
+}
+
+function itemScroll(span){
+    span.parentNode.querySelector('.active')?.classList.remove('active')
+    span.classList.toggle('active')
 }
