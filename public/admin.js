95ccd6610a775531abc7950bbeca18b9dca2de91
EV1L46rus <vislik46rus@gmail.com>
2022-10-03 06:05:47 +0300

    administrator redact

--- public/admin.js
+++ public/admin.js
@@ -1,8 +1,8 @@
 window.addEventListener('resize', closeNavigation);
 
 function openNavigation() {
-    document.querySelector('.navigation-menu').classList.toggle('active')
-    document.querySelector('.blur').classList.toggle('active')
+    document.querySelector('.navigation-menu').classList.add('active')
+    document.querySelector('.blur-add').classList.add('active')
 }
 
 function closeNavigation() {
@@ -17,11 +17,32 @@ function fullOrderMenu(div){
     div.parentNode.querySelector('.full').classList.toggle('active')
 }
 
+function closeMenu() {
+    document.querySelector('.blur-add').classList.remove('active')
+    document.querySelector('.navigation-menu.active')?.classList.remove('active')
+    document.querySelector('.add-admin-menu.active')?.classList.remove('active')
+}
+
+function menuOpen(){
+    document.querySelector('.blur-add').classList.add('active')
+    document.querySelector('.add-admin-menu').classList.add('active')
+}
+
+function showInput(btn){
+    if (btn.parentNode.querySelector('input').type === 'text'){
+        btn.parentNode.querySelector('input').type = 'password'
+    }
+    else if (btn.parentNode.querySelector('input').type === 'password'){
+        btn.parentNode.querySelector('input').type = 'text'
+    }
+    
+}
+
 function switchScreen(div){
     let resultText = div.textContent
     document.querySelector('.body').querySelector('.active')?.classList.remove('active')
     document.querySelector('.navigation-menu').classList.remove('active')
-    document.querySelector('.blur').classList.remove('active')
+    document.querySelector('.blur-add').classList.remove('active')
 
 
     if (resultText === 'Панель управления') {
@@ -30,5 +51,9 @@ function switchScreen(div){
     
     if (resultText === 'Заказы') {
         document.querySelector('.orders').classList.add('active')
+    }
+
+    if (resultText === 'Аккаунты') {
+        document.querySelector('.users').classList.add('active')
     }
 }
